acd52d5583e438543abe4152cbac93b0e51d3aad
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-03 21:18:03 +0300

    fix

---
+++ source/conf.py
@@ -0,0 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Vjik Test'
+copyright = '2021, Graziella'
+author = 'Graziella'
+
+release = '1.0.0'
+version = '1.0.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'myst_parser',
+]
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
